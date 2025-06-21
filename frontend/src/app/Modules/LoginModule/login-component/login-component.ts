666e89c2ae4da7535c75104da6c8475d390d6c7d
Muhammad Salman Amir <mmsa155j3@gmail.com>
2025-06-21 21:05:47 +0500

    login

--- frontend/src/app/Modules/LoginModule/login-component/login-component.ts
+++ frontend/src/app/Modules/LoginModule/login-component/login-component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
   templateUrl: './login-component.html',
-  styleUrl: './login-component.css'
+  styleUrls: ['./login-component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      gsap.from('#loginCard', {
+        duration: 1.2,
+        opacity: 0,
+        scale: 0.8,
+        rotationX: -70,
+        rotationY: -30,
+        transformOrigin: 'center center',
+        ease: 'power3.out',
+      });
+
+      gsap.to('#loginCard', {
+        delay: 1.2,
+        scale: 1.05,
+        ease: 'bounce.out',
+        duration: 0.6,
+      });
+    }
+  }
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts}", // Scan all Angular HTML + TS files
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
